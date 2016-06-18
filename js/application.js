412ad95e56796ac377848973cff3f1a12492531b
Asutosh Palai <asupalai@gmail.com>
2016-06-18 15:16:03 +0530

    Added cover image to jumbotron

--- js/application.js
+++ js/application.js
@@ -1,1 +1,8 @@
 // Your JavaScript
+$(function() {
+  $('.container').find('img').each(function(){
+    var parent = $(this).parent();
+    var imgClass = (this.width/this.height > parent.width()/parent.height()) ? 'wide' : 'tall';
+    $(this).addClass(imgClass);
+  })
+});

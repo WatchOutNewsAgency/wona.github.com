a42ed793c1974cc89862fa3998d829c44f983ca8
Asutosh Palai <asupalai@gmail.com>
2016-06-18 22:58:48 +0530

    Working till listing of post

--- js/application.js
+++ js/application.js
@@ -1,6 +1,6 @@
 // Your JavaScript
 $(function() {
-  $('.container').find('img').each(function(){
+  $('.img-center-fill').each(function(){
     var parent = $(this).parent();
     var imgClass = (this.width/this.height > parent.width()/parent.height()) ? 'wide' : 'tall';
     $(this).addClass(imgClass);

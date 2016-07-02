ba57109502393ee732ef314b484d6d1b6257daa7
Ketan Gupta <ketan19972010@gmail.com>
2016-07-03 00:37:01 +0530

    Small bug fix

--- js/application.js
+++ js/application.js
@@ -1,6 +1,6 @@
 // Your JavaScript
 $(function() {
-  $('.img-center-fill').each(function(){
+  $('.img-center-fill').load(function(){
     var parent = $(this).parent();
     var imgClass = (this.width/this.height > parent.width()/parent.height()) ? 'wide' : 'tall';
     $(this).addClass(imgClass);

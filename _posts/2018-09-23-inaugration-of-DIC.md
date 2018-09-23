f95d1b76a842947e07e0520757c61cefd5e5966b
hritvi <hritvi2801@gmail.com>
2018-09-23 22:43:30 +0530

    Inaugration of DIC

---
+++ _posts/2018-09-23-inaugration-of-DIC.md
@@ -0,0 +1,17 @@
+---
+layout: post
+title: "Inauguration of the Design Innovation Center at IITR"
+tags: [wona, tech]
+image: dic.jpg
+category: tech
+excerpt: "The Design Innovation Centre (DIC) at IIT Roorkee was inaugurated on Monday by Prof. Ajit Chaturvedi"
+---
+
+The Design Innovation Centre (DIC) at IIT Roorkee was inaugurated on Monday by Prof. Ajit Chaturvedi, Director IITR, along with Prof. M Parida, Dean SRIC IITR, Prof. PVM Rao from IIT Delhi and Prof. Praduman Vyas, Director NID Ahmedabad (connecting to the event via skype) as Chief Guests. The DIC, or Navonmesh, was recently been approved by the MHRD with a budget outlay of Rs. 10 crore. 
+The DIC will function under a “hub and spoke” model, wherein IIT Roorkee will act as the “hub”, and the “spokes” i.e. IIM Kashipur, NIT Uttarakhand and CoT, GBPUAT, Pantnagar will be having their own proposals mostly to address local problems. The DIC basically would be inviting proposals for innovative solutions that address issues of the Himalayan region, followed by awarding financial support to the most promising designs to help them yield marketable products or technologies. These proposals will then be considered for registration under Intellectual Property Rights and for commercialization. Some examples of projects might include optimizing agricultural tools such as crop harvester for hilly terrain, and developing assistive devices for those with mobility impairment.
+According to Prof. Apurbba Kumar Sharma, Coordinator & Principal Investigator(PI), the activities of DIC will be carried out under 3 major categories:
+ i) Supporting innovative prospects and projects of faculty members and students of the institute through financial support, 
+ii) Academic activities under which there are plans to introduce 2 academic courses of Masters of industrial design (MDes.) and Masters of Innovation Management (MIM) and organisation of workshops and crash courses for modular designs and innovations. Some laboratory facilities will be developed to compliment the facilities in the institute. Apart from this internships to students from IIT roorkee as well as from outside the institute will be awarded for short terms. 
+iii) Outreach Programs like P2P (Prayogshala to Prayogshetra), U2U (Udbhavan to Utpadan) and COMAL (COmmon MAn to Laboratory), under which innovations at the laboratory scale will be taken to the common man. Apart from these, any members of the public who have innovative ideas can get mentorships and follow ups at the DIC. 
+The Design Innovations Center’s scheme was rolled out in 2014 by the MHRD along with open design school and National Design Innovation network. It started with setting up of 4 DICs across india of which IIT Delhi and IIT BHU were a member. Prof. PVM Rao and Prof. Praduman Vyas who contributed very significantly in formulating these schemes were present in the event. 
+A core team of 5 faculty members of IITR was formed under the leadership of Prof. Manoranjan Parida, Dean SRIC when they received a letter regarding MHRD’s plans for setting up a DIC here in 2017. The three associated institutes joined subsequently.
--- js/application.js
+++ js/application.js
@@ -1,116 +1,120 @@
-// Your JavaScript
-$(function() {
-  function gradeImage(){
-    var parent = $(this).parent();
-    var imgClass = (this.width/this.height > parent.width()/parent.height()) ? 'wide' : 'tall';
-    $(this).addClass(imgClass);
-    console.log(this);
-  }
+// // Your JavaScript
+// $(function() {
+//   function gradeImage(){
+//     var parent = $(this).parent();
+//     var imgClass = (this.width/this.height > parent.width()/parent.height()) ? 'wide' : 'tall';
+//     $(this).addClass(imgClass);
+//     console.log(this);
+//   }
 
-  $('.img-center-fill').each(function () {
-    if(this.complete) {
-      gradeImage.call(this);
-    }
-    else {
-      this.addEventListener('load', gradeImage);
-    }
-  });
+//   $('.img-center-fill').each(function () {
+//     if(this.complete) {
+//       gradeImage.call(this);
+//     }
+//     else {
+//       this.addEventListener('load', gradeImage);
+//     }
+//   });
 
-  /*
-  var switchPos = $('#new-articles').position().top + $('#new-articles').height();
+//   /*
+//   var switchPos = $('#new-articles').position().top + $('#new-articles').height();
 
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll >= switchPos) {
-      $(".navbar-wrapper").addClass("darken");
+//   $(window).scroll(function() {
+//     var scroll = $(window).scrollTop();
+//     if (scroll >= switchPos) {
+//       $(".navbar-wrapper").addClass("darken");
 
-    } else {
-      $(".navbar-wrapper").removeClass("darken");
-    }
-  });
-  */
+//     } else {
+//       $(".navbar-wrapper").removeClass("darken");
+//     }
+//   });
+//   */
 
-  ;(function($, window, document, undefined) {
-    'use strict';
+//   ;(function($, window, document, undefined) {
+//     'use strict';
 
-    var elSelector    = '.navbar-wrapper',
-      elClassHidden = 'navbar--hidden',
-      throttleTimeout = 250,
-      $element    = $( elSelector  );
+//     var elSelector    = '.navbar-wrapper',
+//       elClassHidden = 'navbar--hidden',
+//       elextranav = '.extra-nav',
+//       elextranavHidden='.extra-nav-hidden', 
+//       throttleTimeout = 250,
+//       $element    = $( elSelector  );
+//       $nav = $( elextranavHidden )
 
-    if(!$element.length) return true;
+//     if(!$element.length) return true;
 
-    var $window     = $( window  ),
-      wHeight     = 0,
-      wScrollCurrent  = 0,
-      wScrollBefore = 0,
-      wScrollDiff   = 0,
-      $document   = $( document  ),
-      dHeight     = 0,
+//     var $window     = $( window  ),
+//       wHeight     = 0,
+//       wScrollCurrent  = 0,
+//       wScrollBefore = 0,
+//       wScrollDiff   = 0,
+//       $document   = $( document  ),
+//       dHeight     = 0,
 
-      throttle = function(delay, fn) {
-      var last, deferTimer;
-      return function() {
-        var context = this, args = arguments, now = +new Date;
-        if( last && now < last + delay  ) {
-          clearTimeout( deferTimer  );
-          deferTimer = setTimeout( function(){ last = now; fn.apply( context, args  );  }, delay  );
+//       throttle = function(delay, fn) {
+//       var last, deferTimer;
+//       return function() {
+//         var context = this, args = arguments, now = +new Date;
+//         if( last && now < last + delay  ) {
+//           clearTimeout( deferTimer  );
+//           deferTimer = setTimeout( function(){ last = now; fn.apply( context, args  );  }, delay  );
 
-        }
-        else {
-          last = now;
-          fn.apply( context, args  );
-        }
-      };
-    };
+//         }
+//         else {
+//           last = now;
+//           fn.apply( context, args  );
+//         }
+//       };
+//     };
 
-    $window.on( 'scroll', throttle( throttleTimeout, function() {
-      dHeight     = $document.height();
-      wHeight     = $window.height();
-      wScrollCurrent  = $window.scrollTop();
-      wScrollDiff   = wScrollBefore - wScrollCurrent;
+//     $window.on( 'scroll', throttle( throttleTimeout, function() {
+//       dHeight     = $document.height();
+//       wHeight     = $window.height();
+//       wScrollCurrent  = $window.scrollTop();
+//       wScrollDiff   = wScrollBefore - wScrollCurrent;
 
-      if(wScrollCurrent <= 0) { // scrolled to the very top; element sticks to the top
-        $element.removeClass(elClassHidden);
-        $element.removeClass(elClassTop);
-      }
+//       if(wScrollCurrent <= 0) { // scrolled to the very top; element sticks to the top
+//         $element.removeClass(elClassHidden);
+//         $element.removeClass(elClassTop);
+//         $nav.addClass(elextranav);
+//       }
 
-      else if(wScrollDiff > 10 && $element.hasClass(elClassHidden)) // scrolled up; element slides in
-        $element.removeClass( elClassHidden  );
+//       else if(wScrollDiff > 10 && $element.hasClass(elClassHidden)) // scrolled up; element slides in
+//         $element.removeClass( elClassHidden  );
 
-      else if(wScrollDiff < -10) {// scrolled down
-          if(wScrollCurrent + wHeight >= dHeight && $element.hasClass(elClassHidden)) // scrolled to the very bottom; element slides in
-            $element.removeClass( elClassHidden  );
+//       else if(wScrollDiff < -10) {// scrolled down
+//           if(wScrollCurrent + wHeight >= dHeight && $element.hasClass(elClassHidden)) // scrolled to the very bottom; element slides in
+//             $element.removeClass( elClassHidden  );
 
-          else // scrolled down; element slides out
-            $element.addClass( elClassHidden  );
+//           else // scrolled down; element slides out
+//             $element.addClass( elClassHidden  );
 
-        }
+//         }
 
-        wScrollBefore = wScrollCurrent;
-    }));
+//         wScrollBefore = wScrollCurrent;
+//     }));
 
 
-  })( jQuery, window, document  );
-});
+//   })( jQuery, window, document  );
+// });
 
-function onYouTubeIframeAPIReady() {
-  $('.youtube-video').each(function() {
-    var id = $(this).data('id');
+// function onYouTubeIframeAPIReady() {
+//   $('.youtube-video').each(function() {
+//     var id = $(this).data('id');
 
-    var player = new YT.Player('youtube-video-' + id, {
-      events: {
-        'onStateChange': onPlayerStateChange
-      }
-    });
+//     var player = new YT.Player('youtube-video-' + id, {
+//       events: {
+//         'onStateChange': onPlayerStateChange
+//       }
+//     });
 
-    function onPlayerStateChange(event) {
-      if(event.data === YT.PlayerState.ENDED || event.data === YT.PlayerState.PAUSED)
-        $('#youtube-title-' + id).show();
-      else
-        $('#youtube-title-' + id).hide();
-    }
+//     function onPlayerStateChange(event) {
+//       if(event.data === YT.PlayerState.ENDED || event.data === YT.PlayerState.PAUSED)
+//         $('#youtube-title-' + id).show();
+//       else
+//         $('#youtube-title-' + id).hide();
+//     }
 
-  });
-}
+//   });
+// }
 
